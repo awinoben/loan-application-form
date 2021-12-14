394144d743ec79a92e14c722949abad096b74a76
ben <papasofts@gmail.com>
2021-12-14 08:41:36 +0300

    :package: Updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<swedbank-loan-form>'));
+app.use(express.static(__dirname + '/dist/swedbank-loan-form'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<swedbank-loan-form>/index.html'));});
+'/dist/swedbank-loan-form/index.html'));});
 app.listen(process.env.PORT || 8080);
